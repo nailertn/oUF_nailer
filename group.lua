b8c4b6fa6e50d523b31ce5eb0844bacac59a884e
nailertn <your@email.com>
2012-09-23 15:36:13 +0200

    basic spawn functions

--- group.lua
+++ group.lua
@@ -7,3 +7,33 @@ local function style(object, unit)
 end
 
 oUF:RegisterStyle(layout_name .. ' group style')
+
+local width = 100
+local height = 70
+local offset = 4
+
+oUF:Factory(function()
+	oUF:SetActiveStyle(layout_name .. ' solo style')
+	
+	layout.groups = {}
+	
+	for group_index = 1, NUM_RAID_GROUPS do
+		local group = oUF:SpawnHeader(nil, nil, nil,
+			'groupFilter', group_index,
+			'showSolo', true,
+			'showParty', true,
+			'showPlayer', true,
+			'showRaid', true,
+			'yOffset', -offset,
+			
+			'oUF-initialConfigFunction',
+				'self:SetWidth(' .. width .. ')' ..
+				'self:SetHeight(' .. height .. ')'
+		)
+		
+		tinsert(layout.groups, group)
+		group:Show()
+	end
+	
+	layout.update_groups()
+end)
--- solo.lua
+++ solo.lua
@@ -7,3 +7,17 @@ local function style(object, unit)
 end
 
 oUF:RegisterStyle(layout_name .. ' solo style')
+
+local width, height = 300, 30
+
+oUF:Factory(function()
+	oUF:SetActiveStyle(layout_name .. ' solo style')
+	
+	local player = oUF:Spawn('player')
+	player:SetSize(width, height)
+	player:SetPoint('center', -400, 0)
+	
+	local target = oUF:Spawn('target')
+	target:SetSize(width, height)
+	target:SetPoint('center', 400, 0)
+end)
